853f5cad82b2a4c9213380feee0062e6dfada8f8
SHAH PARAM RAJESH <paramshah1906@gmail.com>
2026-09-11 16:27:01 +0530

    Add Halloumi Boxes [A] from Codeforces

---
+++ 1903/A - Halloumi Boxes/solution.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,k;
+	    cin>>n>>k;
+	    vector<int> a(n);
+	    for (int i=0;i<n;i++) cin>>a[i];
+	    if (k==1){
+	        bool flag=true;
+	        for (int i=1;i<n;i++){
+	            if (a[i]<a[i-1]){
+	                flag=false;
+	                break;
+	            }
+	        }
+	        if (flag) cout<<"YES
+";
+	        else cout<<"NO
+";
+	    }
+	    else cout<<"YES
+";
+	}
+return 0;
+}
