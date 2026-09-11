94e6fba93a131bc6d8ff6ada5123d6e511fe7bce
SHAH PARAM RAJESH <paramshah1906@gmail.com>
2026-09-11 16:37:13 +0530

    Add Lunatic Never Content [B] from Codeforces

---
+++ 1826/B - Lunatic Never Content/solution.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+ 
+#define PB push_back
+#define F first
+#define S second
+ 
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+ 
+#define DEBUG(x) cerr<<#x<<":"<<x<<"
+";
+ 
+void solve()
+{
+    ll n;
+    cin>>n;
+    vector<ll> a(n);
+    for (ll i=0;i<n;i++) cin>>a[i];
+    
+    ll ans=0;
+    for (ll i=0;i<n/2;i++){
+        ans=__gcd(ans,abs(a[i]-a[n-i-1]));
+    }
+    cout<<ans<<"
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
