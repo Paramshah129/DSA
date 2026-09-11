386f2384acc7407999b414e79f8de30c20bc3e6b
SHAH PARAM RAJESH <paramshah1906@gmail.com>
2026-09-11 16:35:25 +0530

    Add Bogosort [B] from Codeforces

---
+++ 1312/B - Bogosort/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+int main() {
+	ll t;
+	cin>>t;
+	while(t--){
+	    ll n;
+	    cin>>n;
+	    vector<ll> a(n);
+	    for (ll i=0;i<n;i++) cin>>a[i];
+	    
+	    sort(a.rbegin(),a.rend());
+	    
+	    for (ll i=0;i<n;i++) cout<<a[i]<<" ";
+	    cout<<"
+";
+	}
+return 0;
+}
