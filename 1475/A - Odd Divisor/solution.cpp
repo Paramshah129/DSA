63f8ebcd662c8d5b21cf00baf5c5314435066a27
SHAH PARAM RAJESH <paramshah1906@gmail.com>
2026-09-11 16:31:53 +0530

    Add Odd Divisor [A] from Codeforces

---
+++ 1475/A - Odd Divisor/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+int main() {
+	ll t;
+	cin>>t;
+	while(t--){
+	    ll n;
+	    cin>>n;
+	    if (((n)&(n-1))!=0) cout<<"Yes
+";
+	    else cout<<"No
+";
+	}
+}
