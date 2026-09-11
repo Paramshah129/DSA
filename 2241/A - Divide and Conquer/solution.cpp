694023c44d85b0d0602b6e4fb8a06d0a8ecaba1a
SHAH PARAM RAJESH <paramshah1906@gmail.com>
2026-09-11 16:33:55 +0530

    Add Divide and Conquer [A] from Codeforces

---
+++ 2241/A - Divide and Conquer/solution.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+int main() {
+	ll t;
+	cin>>t;
+	while(t--){
+	    ll x,y;
+	    cin>>x>>y;
+	    if (x%y==0) cout<<"YES
+";
+	    else cout<<"No
+";
+	}
+return 0;
+}
