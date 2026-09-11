16b24359c28d8b04d62825dc6c2d7a7236561fa9
SHAH PARAM RAJESH <paramshah1906@gmail.com>
2026-09-11 16:28:03 +0530

    Add Array Coloring [A] from Codeforces

---
+++ 1857/A - Array Coloring/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    vector<int> a(n);
+	    for (int i=0;i<n;i++) cin>>a[i];
+	    int sum=0;
+	    for (int i=0;i<n;i++) sum+=a[i];
+	    if (sum%2==0) cout<<"Yes
+";
+	    else cout<<"No
+";
+	}
+return 0;
+}
